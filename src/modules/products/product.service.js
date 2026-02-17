88c749567d35a00974b5fc4456b9665d692909c2
Ratu <ratukf@gmail.com>
2026-02-17 11:28:44 +0700

    refactor(service): use better product service naming

--- src/modules/products/product.service.js
+++ src/modules/products/product.service.js
@@ -1,18 +1,18 @@
 // Services
-import prisma from '../../lib/prisma';
+import prisma from '../../lib/prisma.js';
 
 // Post
-const createProduct = async (data) => {
+const createProductService = async (data) => {
   return prisma.product.create({ data });
 };
 
 // Fetch all
-const getAllProducts = async () => {
+const getAllProductsService = async () => {
   return prisma.product.findMany({ orderBy: { createdAt: 'desc' } });
 };
 
 // Fetch one
-const getProductById = async (id) => {
+const getProductByIdService = async (id) => {
   const product = await prisma.product.findUnique({
     where: { id: Number(id) },
   });
@@ -27,8 +27,8 @@ const getProductById = async (id) => {
 };
 
 // Patch
-const updateProduct = async (id, data) => {
-  await getProductById(id);
+const updateProductService = async (id, data) => {
+  await getProductByIdService(id);
 
   return prisma.product.update({
     where: { id: Number(id) },
@@ -37,17 +37,17 @@ const updateProduct = async (id, data) => {
 };
 
 // Delete
-const deleteProduct = async (id) => {
-  await getProductById(id);
+const deleteProductService = async (id) => {
+  await getProductByIdService(id);
   return prisma.product.delete({
     where: { id: Number(id) },
   });
 };
 
-module.exports = {
-  createProduct,
-  getAllProducts,
-  getProductById,
-  updateProduct,
-  deleteProduct,
+export {
+  createProductService,
+  getAllProductsService,
+  getProductByIdService,
+  updateProductService,
+  deleteProductService,
 };
